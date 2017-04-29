665f8720192bbf61a767515b7e535be2e9824840
timlee201 <timlee201@gmail.com>
2017-04-29 21:34:07 +0100

    Set up MZN models

---
+++ spring-boot-mzn/src/main/java/mzn/models/Users.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mzn.models;
+
+/**
+ *
+ * @author Owner
+ */
+public class Users {
+    
+}
